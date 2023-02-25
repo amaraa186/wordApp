1c287dd6f4c76f9b020bfb0d06b4bc5b46a40659
Amaraa <oamraa62@gmail.com>
2023-02-25 17:39:55 +0800

    .

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import SplashScreen from './src/components/SplashScreen/Splash';
+import HomeScreen from './src/components/HomeScreen/Home';
+import WordScreen from './src/components/WordScreen/Word';
+
+const Stack = createStackNavigator();
+
+function Stacks() {
+  return (
+    <Stack.Navigator
+        initialRouteName="Splash"
+    >
+        <Stack.Screen name="Splash" component={SplashScreen} options={{
+          headerShown: false,
+          animationEnabled: false,
+        }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{
+          headerShown: false,
+          animationEnabled: false,
+        }} />
+        <Stack.Screen name="Word" component={WordScreen} />
+    </Stack.Navigator> 
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stacks />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/WordScreen/Word.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, View } from 'react-native';
+
+const Word = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default Word;
